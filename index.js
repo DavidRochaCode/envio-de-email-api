d5ef5b884a3fc402f9f8cf5995b0bd69f5bb001f
David Rocha <manoeudavi3@gmail.com>
2023-07-18 18:18:37 -0300

    styled

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-const express = require("express")
-const nodemailer = require("nodemailer")
-require('dotenv').config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-
-// Configuração do provedor 
+// Configuração do provedor
+//Habilitar 2 step verification e criar um app password no seu provedor, antes de rodar a api.
 let smtp = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 //lista de emails que quero enviar ( buscar do banco de dados)
 const lista = [
-    { nome: "David", email: "manoeudavi3@gmail.com" },
-    { nome: "Adalto", email: "adalto21lopes13rocha@gmail.com" }
+  { nome: "David", email: "manoeudavi3@gmail.com" },
+  { nome: "Adalto", email: "adalto21lopes13rocha@gmail.com" },
 ];
 
-
-// chamar a configuração do e-mail e passar como parâmetro para quem vai ser enviado.
+// Chamar a configuração do e-mail e passar como parâmetro para quem vai ser enviado.
 //Disparar e-mail
-const emailPromises = lista.map(pessoa => {
-    const emailConfig = {
-        from: "manoeudavi20@gmail.com",
-        to: pessoa.email,
-        subject: "Último teste",
-        html: `<p>Olá, ${pessoa.nome}, esse e-mail é para confirmar ...</p>`
-    };
+const emailPromises = lista.map((pessoa) => {
+  const emailConfig = {
+    from: "manoeudavi20@gmail.com",
+    to: pessoa.email,
+    subject: "Teste da API",
+    html: `<p>Olá, ${pessoa.nome}, esse e-mail é para confirmar ...</p>`,
+  };
 
-    return smtp.sendMail(emailConfig);
+  return smtp.sendMail(emailConfig);
 });
 
 Promise.all(emailPromises)
-    .then(results => {
-        console.log(results);
-        smtp.close();
-    })
-    .catch(error => {
-        console.log("Erro ao enviar os emails:", error);
-        smtp.close();
-    });
+  .then((results) => {
+    console.log(results);
+    smtp.close();
+  })
+  .catch((error) => {
+    console.log("Erro ao enviar os emails:", error);
+    smtp.close();
+  });
 
-
-
-
-        
-const app = express()
+const app = express();
 app.listen(3003, () => {
-    console.log("Servidor iniciado")
-})
-  
+  console.log("Servidor iniciado");
+});
